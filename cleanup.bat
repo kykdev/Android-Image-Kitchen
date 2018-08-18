9b94a3d33be6589b6a9ef448e24d189d8e681a07
kykdev <kykint@naver.com>
2018-08-18 13:46:30 +0900

    More specific cleanup targets
    Signed-off-by: kykdev <kykint@naver.com>

--- cleanup.bat
+++ cleanup.bat
@@ -8,7 +8,7 @@ if "%~1" == "--help" echo usage: cleanup.bat & goto end
 "%bin%"\chmod -fR +rw ramdisk split_img >nul 2>&1
 rd /s /q ramdisk >nul 2>&1
 rd /s /q split_img >nul 2>&1
-del *new.* *original.* >nul 2>&1
+del *new.img *new.cpio.gz >nul 2>&1
 echo %cmdcmdline% | findstr /i pushd >nul
 if not errorlevel 1 echo Working directory cleaned. & echo.
 
